b4f7b4c50c4416ac797fe620be9c4b2afc7c5d2c
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-13 08:58:21 +0300

    style : format files

--- src/main/java/com/school/hei/repository/UploadRepository.java
+++ src/main/java/com/school/hei/repository/UploadRepository.java
@@ -1,8 +1,7 @@
 package com.school.hei.repository;
 
 import com.school.hei.repository.model.JUpload;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UploadRepository extends JpaRepository<JUpload, UUID> {}
