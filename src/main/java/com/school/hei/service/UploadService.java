0956600006e01d691d745e9bb0168cd1305848f9
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-13 09:50:00 +0300

    feat: create dto and service for Upload

---
+++ src/main/java/com/school/hei/service/UploadService.java
@@ -0,0 +1,47 @@
+package com.school.hei.service;
+
+import com.school.hei.dto.UploadRequest;
+import com.school.hei.file.bucket.BucketComponent;
+import com.school.hei.mapper.UploadMapper;
+import com.school.hei.model.Upload;
+import com.school.hei.repository.UploadRepository;
+import com.school.hei.repository.model.JUpload;
+import java.io.File;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@AllArgsConstructor
+public class UploadService {
+  private final UploadRepository repository;
+  private final UploadMapper mapper;
+  private final BucketComponent bucketComponent;
+
+  @SneakyThrows
+  public Upload uploadFile(UploadRequest request) {
+    MultipartFile file = request.file();
+    String originalFilename = file.getOriginalFilename();
+    String bucketKey = "uploads/" + UUID.randomUUID() + "_" + originalFilename;
+
+    File tempFile = File.createTempFile("upload-", originalFilename);
+    file.transferTo(tempFile);
+
+    bucketComponent.upload(tempFile, bucketKey);
+
+    tempFile.delete();
+
+    JUpload jUpload =
+        JUpload.builder()
+            .id(UUID.randomUUID())
+            .fileName(originalFilename)
+            .email(request.email())
+            .createdAt(Instant.now())
+            .build();
+
+    return mapper.toModel(repository.save(jUpload));
+  }
+}
