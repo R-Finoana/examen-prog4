2b38fad4839ceeec127739f9530f6169f9f2a2c7
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-13 09:10:48 +0300

    feat: add missing annnotations for JUpload

--- src/main/java/com/school/hei/repository/model/JUpload.java
+++ src/main/java/com/school/hei/repository/model/JUpload.java
@@ -3,8 +3,15 @@ package com.school.hei.repository.model;
 import jakarta.persistence.*;
 import java.time.Instant;
 import java.util.UUID;
+
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 @Entity
 @Table(name = "upload")
 public class JUpload {
